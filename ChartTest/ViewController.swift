fa0084b932d50cfd4935142936632b4b59d16e57
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-06-19 18:30:12 +0600

    init

--- ChartTest/ViewController.swift
+++ ChartTest/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var months: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(dataPoints: months, values: unitsSold)
+        
+    }
+    
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(x: values[i], y: Double(i))
+            dataEntries.append(dataEntry)
+        }
+        let c1 = UIColor(hex: 0x3EC1C1)
+        let c2 = UIColor(hex: 0x209999)
+        let c3 = UIColor(hex: 0x093B64)
+        let c4 = UIColor(hex: 0x005C75)
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: nil)
+        chartDataSet.colors = [c1, c2, c3, c4]
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        barChartView.data = chartData
     }
 
 
 }
 
+extension UIColor {
+    
+    convenience init(hex: Int) {
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+    }
+    
+}
