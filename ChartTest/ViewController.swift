181318dce36170520f249221d2a9c44fa7ce0acf
Md Sazzad Islam <sazzad.islam@dsinnovators.com>
2017-06-21 16:38:31 +0600

    update

--- ChartTest/ViewController.swift
+++ ChartTest/ViewController.swift
@@ -14,13 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var barChartView: BarChartView!
     @IBOutlet weak var pieChartLeft: PieChartView!
     @IBOutlet weak var pieChartRight: PieChartView!
-    @IBOutlet weak var pieContainer: UIView!
-    @IBOutlet weak var leftPieContainer: UIView!
-    @IBOutlet weak var rightPieContainer: UIView!
-    @IBOutlet weak var pieChartLeftContainerWidth: NSLayoutConstraint!
-    @IBOutlet weak var pieChartLeftContainerHeight: NSLayoutConstraint!
-    @IBOutlet weak var pieChartRightContainerWidth: NSLayoutConstraint!
-    @IBOutlet weak var pieChartRightContainerHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var pieLeftHeight: NSLayoutConstraint!
+    @IBOutlet weak var pieLeftWidth: NSLayoutConstraint!
+    
+    @IBOutlet weak var pieRightWidth: NSLayoutConstraint!
+    @IBOutlet weak var pieRightHeight: NSLayoutConstraint!
     
     
     
@@ -28,27 +27,27 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        pieChartLeftContainerWidth.constant = pieContainer.frame.width / 2
-        pieChartLeftContainerHeight.constant = pieContainer.frame.height - 20
-        pieChartRightContainerWidth.constant = pieContainer.frame.width / 2
-        pieChartRightContainerHeight.constant = pieContainer.frame.height - 20
+        pieLeftWidth.constant = self.view.frame.width / 2
+        pieLeftHeight.constant = self.view.frame.width / 2
+        pieRightWidth.constant = self.view.frame.width / 2
+        pieRightHeight.constant = self.view.frame.width / 2
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"]
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0]
-        
-        setBarChart(dataPoints: months, values: unitsSold)
-        setPieChartLeft(dataPoints: months, values: unitsSold)
+        let unitsSoldLp = [20, 40, 10, 20, 10.0]
         let unitsSoldRp = [100.0]
+        
+        setBarChart(values: unitsSold)
+        setPieChartLeft(values: unitsSoldLp)
         setPieChartRight(values: unitsSoldRp)
         
     }
     
-    func setPieChartRight(values: [Double]) {
-        pieChartRight.noDataText = "You need to provide data for the chart."
+    private func setPieChartRight(values: [Double]) {
+        pieChartRight.setExtraOffsets(left: 0.0, top: 0.0, right: -30.0, bottom: -30.0)
+        pieChartRight.noDataText = ""
         var dataEntries: [PieChartDataEntry] = []
         for i in 0..<1 {
             let dataEntry = PieChartDataEntry(value: values[i])
@@ -57,6 +56,7 @@ class ViewController: UIViewController {
         let c1 = UIColor(hex: 0x3EC1C1)
         let chartDataSet = PieChartDataSet(values: dataEntries, label: nil)
         chartDataSet.form = .empty
+        chartDataSet.drawValuesEnabled = false
         chartDataSet.colors = [c1]
         let chartData = PieChartData(dataSet: chartDataSet)
         pieChartRight.data = chartData
@@ -66,29 +66,16 @@ class ViewController: UIViewController {
         pieChartRight.transparentCircleColor = nil
         pieChartRight.holeColor = nil
         pieChartRight.holeRadiusPercent = 0.7
-        pieChartLeft.transparentCircleRadiusPercent = -10
         pieChartRight.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
         pieChartRight.chartDescription = nil
         setRightPieChartCenterText()
     }
     
-    private func setRightPieChartCenterText(){
-        pieChartRight.setExtraOffsets(left: 1.0, top: 1.0, right: -30.0, bottom: -1000.0)
-        let normalStyle = "#normal{color: #ffffff; font-size: 16; text-align: center;}"
-        let firstLineStyle = "#blue{color: #ffffff; font-weight: Bold; font-size: 14; text-align: center;}"
-        let percentStyle = "#percent{color: #ffffff;font-size: 20; text-align: center;}"
-        let style = "<style type=\"text/css\">\(normalStyle)\(firstLineStyle)\(percentStyle)</style>"
-        let firstLine = "<div id=\"normal\">Your offline data</div>"
-        let secondLine = "<div id=\"normal\">is <span id=\"percent\">100%</span> Synced</div>"
-        let str = "\(style)\(firstLine)\(secondLine)".html2AttStr!
-        pieChartRight.centerAttributedText = str
-    }
-    
-    
-    func setPieChartLeft(dataPoints: [String], values: [Double]) {
+    private func setPieChartLeft(values: [Double]) {
+        pieChartLeft.setExtraOffsets(left: -30.0, top: 0.0, right: 0.0, bottom: -30.0)
         pieChartLeft.noDataText = ""
         var dataEntries: [PieChartDataEntry] = []
-        for i in 0..<dataPoints.count {
+        for i in 0..<4 {
             let dataEntry = PieChartDataEntry(value: values[i])
             dataEntries.append(dataEntry)
         }
@@ -100,39 +87,50 @@ class ViewController: UIViewController {
         let chartDataSet = PieChartDataSet(values: dataEntries, label: nil)
         chartDataSet.form = .empty
         chartDataSet.colors = [c1, c2, c3, c4]
+        chartDataSet.drawValuesEnabled = false
         let chartData = PieChartData(dataSet: chartDataSet)
         pieChartLeft.data = chartData
         pieChartLeft.transparentCircleColor = nil
         pieChartLeft.holeColor = nil
         pieChartLeft.holeRadiusPercent = 0.7
-        
-        
         pieChartLeft.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
         pieChartLeft.chartDescription = nil
-        
-        
         leftPieChartCenterTextDesign()
     }
     
+    private func setRightPieChartCenterText(){
+        let topFontSize = self.view.frame.width * 3.8 / 100
+        let percentFontSize = self.view.frame.width * 5 / 100
+        let firstLineStyle = "#firstLine{color: #ffffff; font-size: \(topFontSize); font-weight: Bold; text-align: center;}"
+        let secondLineStyle = "#secondLine{color: #ffffff; font-size: \(topFontSize); text-align: center;}"
+        let percentStyle = "#percent{color: #ffffff;font-size: \(percentFontSize); text-align: center;}"
+        let style = "<style type=\"text/css\">\(firstLineStyle)\(secondLineStyle)\(percentStyle)</style>"
+        let firstLine = "<div id=\"firstLine\">Your offline data</div>"
+        let secondLine = "<div id=\"secondLine\">is <span id=\"percent\">100%</span> Synced</div>"
+        let str = "\(style)\(firstLine)\(secondLine)".html2AttStr!
+        pieChartRight.centerAttributedText = str
+        print("fs \(self.view.frame.width * 5 / 100)")
+    }
+    
     private func leftPieChartCenterTextDesign(){
-        let styleNormal = "#normal{color: #ffffff; font-size: 16; text-align: center;}"
-        let styleBlue = "#blue{color: #ffffff; font-weight: Bold; font-size: 18; text-align: center;}"
-        let stylePercent = "#percent{color: #ffffff;font-size: 20; text-align: center;}"
-        let style = "<style type=\"text/css\">\(styleNormal)\(styleBlue)\(stylePercent)</style>"
-        let firstLine = "<div id=\"blue\">Professional</div>"
-        let secondLine = "<div id=\"normal\">info <span id=\"percent\">70%</span></div>"
+        let topFontSize = self.view.frame.width * 5 / 100
+        let bottomFontSize = self.view.frame.width * 4.74 / 100
+        let percentFontSize = self.view.frame.width * 7.5 / 100
+        let styleFirstLine = "#firstLine{color: #ffffff;font-weight: Bold; font-size: \(topFontSize); text-align: center;}"
+        let styleSecondLine = "#secondLine{color: #ffffff; font-size: \(bottomFontSize); text-align: center;}"
+        let stylePercent = "#percent{color: #ffffff;font-size: \(percentFontSize); text-align: center;}"
+        let style = "<style type=\"text/css\">\(styleFirstLine)\(styleSecondLine)\(stylePercent)</style>"
+        let firstLine = "<div id=\"firstLine\">Professional</div>"
+        let secondLine = "<div id=\"secondLine\">info <span id=\"percent\">70%</span></div>"
         let str = "\(style)\(firstLine)\(secondLine)".html2AttStr!
         pieChartLeft.centerAttributedText = str
     }
     
-    
-    
-    
-    func setBarChart(dataPoints: [String], values: [Double]) {
+    func setBarChart(values: [Double]) {
         barChartView.noDataText = ""
         var dataEntries: [BarChartDataEntry] = []
         let marker : [Double] = [100, 200, 300, 400, 500, 600, 700, 800]
-        for i in 0..<dataPoints.count {
+        for i in 0..<7 {
             let dataEntry = BarChartDataEntry(x: values[i], y: marker[i])
             dataEntries.append(dataEntry)
         }
@@ -159,6 +157,8 @@ class ViewController: UIViewController {
         barChartView.rightYAxisRenderer = YAxisRenderer(viewPortHandler: nil, yAxis: nil, transformer: nil)
         //barChartView.xAxisRenderer = XAxisRenderer(viewPortHandler: nil, xAxis: nil, transformer: nil)
         
+        
+        
     }
     
     
@@ -171,7 +171,6 @@ extension String {
 }
 
 extension UIColor {
-    
     convenience init(hex: Int) {
         let components = (
             R: CGFloat((hex >> 16) & 0xff) / 255,
@@ -180,5 +179,4 @@ extension UIColor {
         )
         self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
     }
-    
 }
